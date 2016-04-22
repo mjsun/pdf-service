4ffd6b82cd19f83f8a520a1aa83941e25900697d
mjsun <msun@mingjies-MBP.fios-router.home>
2016-04-21 22:20:42 -0400

    set up pdf server

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ https://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk_server-2.02-mac_osx-10
 If you are using VM, Please install with the following command:
 sudo apt-get install pdftk
 
+
+Install Redis on Macbook Pro:
+
+brew install redis
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var restify = require('restify');
-var cors = require('cors');
 var redis = require('redis');
 var client = redis.createClient();
+var pdfFiller = require('pdffiller');
+var Promise = require('bluebird');
 
 client.on('connect', function(){
     console.log('Redis Connected');
@@ -22,16 +23,41 @@ function guid() {
       s4() + '-' + s4() + s4() + s4();
 }
 
-var channels = [''];
+var channels = ['snapp', 'haven'];
 
 var server = restify.createServer();
-server.use(cors());
 
-server.get('/templates/:id', function(req, res, next){
-    
+server.use(restify.bodyParser());
+server.use(
+    function crossOrigin(req,res,next){
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        return next();
+    }
+); //turn on CORS
+
+server.get('/', function(req, res, next){
+    res.send('Try /template/snapp');
 });
 
 server.post('/templates/:channel', function(req, res, next){
+    var channel = req.params.channel;
+    var body = req.body;
+
+    /**
+     * This is just a mock
+     * */
+    var sourcePdf = '/pdf/template_1.pdf';
+    pdfFiller.generateFieldJson(sourcePdf, null, function(err, fdfData){
+        if(err){
+            res.status(501);
+        }
+        else {
+            //defer.resolve([fdfData, fileName, fileTempPath, data]);
+            res.send(fdfData);
+        }
+    });
+
 });
 
 server.listen(1375, function() {
