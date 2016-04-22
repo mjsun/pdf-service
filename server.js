8c82481cf9fc2c1e56fb97601907b6c43c6c9acc
msun <mingjie@havenlife.com>
2016-04-22 17:43:36 -0400

    added api

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var redis = require('redis');
 var client = redis.createClient();
 var pdfFiller = require('pdffiller');
 var Promise = require('bluebird');
-
+var fs = require('fs');
 client.on('connect', function(){
     console.log('Redis Connected');
 });
@@ -13,7 +13,7 @@ function respond(req, res, next) {
   next();
 }
 
-function guid() {
+function generateId() {
   function s4() {
     return Math.floor((1 + Math.random()) * 0x10000)
         .toString(16)
@@ -28,13 +28,7 @@ var channels = ['snapp', 'haven'];
 var server = restify.createServer();
 
 server.use(restify.bodyParser());
-server.use(
-    function crossOrigin(req,res,next){
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        return next();
-    }
-); //turn on CORS
+server.pre(restify.CORS()); //turn on CORS
 
 server.get('/', function(req, res, next){
     res.send('Try /template/snapp');
@@ -42,22 +36,32 @@ server.get('/', function(req, res, next){
 
 server.post('/templates/:channel', function(req, res, next){
     var channel = req.params.channel;
-    var body = req.body;
+    var body = req.body,
+        file = body.file,
+        fileName = body.name;
 
-    /**
-     * This is just a mock
-     * */
-    var sourcePdf = '/pdf/template_1.pdf';
-    pdfFiller.generateFieldJson(sourcePdf, null, function(err, fdfData){
+    var path = './pdf/' + channel + '/';
+
+    if(!fs.existsSync(path)) {
+        fs.mkdirSync(path);
+    }
+
+    var fileId = generateId();
+    fs.writeFileSync(path + fileName, file);
+    pdfFiller.generateFieldJson(path + fileName, null, function(err, fdfData){
         if(err){
             res.status(501);
         }
         else {
-            //defer.resolve([fdfData, fileName, fileTempPath, data]);
+            client.set(fileId, JSON.stringify(fdfData));
             res.send(fdfData);
         }
     });
 
+});
+
+server.post('/mergefill/:channel', function(req, res, next){
+    var channel = req.params.channel;
 });
 
 server.listen(1375, function() {
