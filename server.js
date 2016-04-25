06698ecc2f19d17f8756547f350a574ac18d1bb0
msun <mingjie@havenlife.com>
2016-04-25 15:57:49 -0400

    added test scripts

--- server.js
+++ server.js
@@ -1,151 +1,172 @@
-var restify = require('restify');
-var redis = require('redis');
-var client = redis.createClient();
-var pdfFiller = require('pdffiller');
-var Promise = require('bluebird');
-var fs = require('fs');
-var PDFMerge = require('pdf-merge');
+exports.StartServer = function(){
 
-client.on('connect', function(){
-    console.log('Redis Connected');
-});
+    var restify = require('restify');
+    var redis = require('redis');
+    var client = redis.createClient();
+    var pdfFiller = require('pdffiller');
+    var Promise = require('bluebird');
+    var fs = require('fs');
+    var FS = require('q-io/fs');
+    var PDFMerge = require('pdf-merge');
+    var _ =require('lodash');
 
-function respond(req, res, next) {
-  res.send('hello ' + req.params.name);
-  next();
-}
+    client.on('connect', function(){
+        console.log('Redis Connected');
+    });
 
-function generateId() {
-  function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000)
-        .toString(16)
-        .substring(1);
-  }
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-      s4() + '-' + s4() + s4() + s4();
-}
+    client.on('error', function(err){
+        console.log('Error ' + err);
+    });
 
-var channels = ['snapp', 'haven'];
-
-var server = restify.createServer();
-
-server.use(restify.bodyParser());
-server.pre(restify.CORS()); //turn on CORS
-
-server.get('/', function(req, res, next){
-    res.send('Try /template/snapp');
-});
-
-server.post('/templates/:channel', function(req, res, next){
-    var channel = req.params.channel;
-    var body = req.body,
-        file = body.file,
-        fileName = body.name;
-
-    var path = './pdf/' + channel + '/';
-
-    if(!fs.existsSync(path)) {
-        fs.mkdirSync(path);
+    function generateId() {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+            s4() + '-' + s4() + s4() + s4();
     }
 
-    var fileId = generateId();
-    fs.writeFileSync(path + fileName, file);
-    pdfFiller.generateFieldJson(path + fileName, null, function(err, fdfData){
-        if(err){
-            throw new Error (err);
+    var channels = ['snapp', 'haven'];
+
+    var server = restify.createServer();
+
+    server.use(restify.bodyParser());
+    server.pre(restify.CORS()); //turn on CORS
+
+    server.get('/', function(req, res, next){
+        res.send('Please choose valid API entry point');
+    });
+
+    server.post('/templates/:channel', function(req, res, next){
+        var channel = req.params.channel;
+        var body = req.body,
+            file = body.file,
+            fileName = body.fileName;
+
+        var path = './pdf/' + channel + '/';
+
+        if(!fs.existsSync(path)) {
+            fs.mkdirSync(path);
         }
-        else {
-            client.set(fileId+'_name', fileName);
-            client.set(fileId+'_tags', JSON.stringify(fdfData));
-            res.send(fdfData);
+
+        var fileId = generateId();
+        var dest = path + fileName;
+
+        FS.write(dest, file)
+            .then(function(){
+                pdfFiller.generateFieldJson(path + fileName, null, function(err, fdfData){
+                    if(err){
+                        throw new Error (err);
+                    }
+                    else {
+                        client.set(fileId+'_name', fileName, function(){
+                            client.set(fileId+'_tags', JSON.stringify(fdfData), function(){
+                                res.send(fileId);
+                            })
+                        });
+                    }
+                });
+            });
+    });
+
+    server.get('templates/:channel/:templateId/:contentType', function(req, res, next){
+        var channel = req.params.channel,
+            templateId = req.params.templateId,
+            contentType = req.params.contentType;
+        var fileDir = './pdf/' + channel + '/';
+
+
+        if(contentType === 'form_mappings'){
+            client.get(templateId+'_tags', function(err, data){
+                res.send(JSON.parse(data));
+            });
+        }
+        else if(contentType === 'template') {
+            client.get(templateId+'_name' ,function(err, fileName){
+                if(err){
+                    throw new Error(err);
+                }
+
+                fs.readFile(fileDir + fileName, function(err, data){
+
+                    if(err) {
+                        throw new Error(err);
+                    }
+                    res.send(data);
+                });
+            });
+
         }
     });
 
-});
+    server.post('/mergefill/:channel', function(req, res, next){
+        var channel = req.params.channel;
+        var body = req.body; //array of template id, order, metadata
+        var allPromises;
+        var dateNow = Date.now();
+        var dirId = generateId();
+        var sourceDir = './pdf/' + channel + '/';
+        var destDir = './pdf/' + channel + '/' + dateNow + '/' + dirId;
+        /*
+         {
+         templateId: '',
+         formOrder: '',
+         mappings: ''
+         }
+         */
+        //sort array
+        body.sort(function(a, b){
+            return a.formOrder - b.formOrder;
+        });
 
-server.get('templates/:channel/:templateId/:contentType', function(req, res, next){
-    var channel = req.params.channel,
-        templateId = req.params.templateId,
-        contentType = req.params.contentType;
-    var fileDir = './pdf/' + form.channel + '/';
+        //loop body
+        allPromises = _.map(body, function(form){
+            //find template
+            var templateName = client.get(form.templateId+'_name');
+            var source = sourceDir + templateName, dest = destDir + templateName;
+            return new Promise(function(resolve, reject){
+                pdfFiller.fillForm(source, dest, form.mappings, function (error) {
+                    if (error) {
+                        reject(error);
+                    }
+                    resolve(dest);
+                });
+            });
+        });
 
-    if(contentType === 'form_mappings'){
-        res.send(JSON.parse(client.get(templateId+'_tags')));
-    }
-    else if(contentType === 'template') {
-        fs.readFile(fileDir+client.get(templateId+'_name'))
+        //merge
+        Promise
+            .all(allPromises)
+            .then(function(generatedPdfs){
+                console.log(generatedPdfs);
+                var pdfMerge = new PDFMerge(generatedPdfs);
+                var mergedPdf = destDir + 'finalMergedFile.pdf';
+                return new Promise(function(resolve, reject){
+                    pdfMerge.asNewFile(mergedPdf).merge(function (error, filePath) {
+                        if (error) {
+                            reject(error);
+                        }
+                        resolve(mergedPdf);
+                    });
+                });
+            })
+            .then(function(mergedFilePath){
+                return FS.read(mergedFilePath);
+            })
             .then(function(data){
                 res.send(data);
             })
             .catch(function(err){
-                throw new Error(err);
+                throw new Error (err);
             });
-    }
-});
-
-server.post('/mergefill/:channel', function(req, res, next){
-    var channel = req.params.channel;
-    var body = req.body; //array of template id, order, metadata
-    var allPromises;
-    var dateNow = Date.now();
-    var dirId = generateId();
-    var sourceDir = './pdf/' + form.channel + '/';
-    var destDir = './pdf/' + form.channel + '/' + dateNow + '/' + dirId;
-    /*
-    {
-        templateId: '',
-        formOrder: '',
-        mappings: ''
-    }
-     */
-
-    //sort array
-    body.sort(function(a, b){
-        return a.formOrder - b.formOrder;
     });
 
-    //loop body
-    allPromises = _.map(body, function(form){
-        //find template
-        var templateName = client.get(form.templateId+'_name');
-        var source = sourceDir + templateName, dest = destDir + templateName;
-        return new Promise(function(resolve, reject){
-            PDFFiller.fillForm(source, dest, form.mappings, function (error) {
-                if (error) {
-                    reject(error);
-                }
-                resolve(dest);
-            });
-        });
+
+    server.listen(1375, function() {
+        console.log('%s listening at %s', server.name, server.url);
     });
 
-    //merge
-    Promise
-        .all(allPromises)
-        .then(function(generatedPdfs){
-            var pdfMerge = new PDFMerge(generatedPdfs);
-            var mergedPdf = destDir + 'finalMergedFile.pdf';
-            return new Promise(function(resolve, reject){
-                pdfMerge.asNewFile(mergedPdf).merge(function (error, filePath) {
-                    if (error) {
-                        reject(error);
-                    }
-                    resolve(mergedPdf);
-                });
-            });
-        })
-        .then(function(mergedFilePath){
-            return fs.readFile(mergedFilePath);
-        })
-        .then(function(data){
-            res.send(data);
-        })
-        .catch(function(err){
-            throw new Error (err);
-        });
-});
+};
 
-
-server.listen(1375, function() {
-  console.log('%s listening at %s', server.name, server.url);
-});
---
+++ test/service_template.js
@@ -0,0 +1,146 @@
+var restify = require('restify');
+var server = require('../server');
+var client = restify.createJsonClient({
+    version: '*',
+    url: 'http://127.0.0.1:1375'
+});
+var _ = require('lodash');
+var fs = require('fs');
+var FS = require('q-io/fs');
+var readTemplate;
+var path = require('path');
+var fileId = [];
+var expect = require('chai').expect;
+var formMapping = {
+    InsuredFirstName: 'Mike',
+    InsuredMiddleName: 'Erwin',
+    InsuredLastName: 'Gap',
+    InsuredSuffix: 'Jr.',
+    OwnerName: 'ChiaChia'
+};
+var sampleTemplates = ['test_template.pdf', 'test_template_1.pdf', 'test_template_2.pdf'];
+var resultIDs = [];
+before(function(done){
+    server.StartServer();
+    done();
+});
+
+describe('PDF services', function() {
+
+    // Test #1
+    describe('200 response check', function() {
+        it('should get a 200 response', function(done) {
+            client.get('/', function(err, req, res, data) {
+                if (err) {
+                    throw new Error(err);
+                }
+                else {
+                    if (data != 'Please choose valid API entry point') {
+                        throw new Error('invalid response from /');
+                    }
+                    done();
+                }
+            });
+        });
+    });
+
+    describe('snapp templates test', function(){
+        before(function(done){
+            fs.readFile('./pdf/test_template.pdf', function(err, data){
+                if(err){
+                    throw new Error(err);
+                }
+                readTemplate = data;
+                done();
+            });
+        });
+
+        it('upload a template',function(done){
+            client.post('/templates/snapp', {fileName: 'test_template.pdf', file: readTemplate}, function(err, req, res, data){
+                if (err) {
+                    throw new Error(err);
+                }
+                else {
+                    FS.exists('./pdf/snapp/test_template.pdf')
+                        .then(function(status){
+                            if(!status){
+                                throw new Error('File is not generated.');
+                            }
+                            fileId.push(data);
+                            done();
+                        });
+                }
+            });
+        });
+
+        it('upload a template 1',function(done){
+            client.post('/templates/snapp', {fileName: 'test_template_1.pdf', file: readTemplate}, function(err, req, res, data){
+                if (err) {
+                    throw new Error(err);
+                }
+                else {
+                    FS.exists('./pdf/snapp/test_template_1.pdf')
+                        .then(function(status){
+                            if(!status){
+                                throw new Error('File is not generated.');
+                            }
+                            fileId.push(data);
+                            done();
+                        });
+                }
+            });
+        });
+
+        it('upload a template 2',function(done){
+            client.post('/templates/snapp', {fileName: 'test_template_2.pdf', file: readTemplate}, function(err, req, res, data){
+                if (err) {
+                    throw new Error(err);
+                }
+                else {
+                    FS.exists('./pdf/snapp/test_template_2.pdf')
+                        .then(function(status){
+                            if(!status){
+                                throw new Error('File is not generated.');
+                            }
+                            fileId.push(data);
+                            done();
+                        });
+                }
+            });
+        });
+
+        it('get a template metadata', function(done){
+            client.get('/templates/snapp/' + fileId[0] + '/form_mappings', function(err, req, res, data){
+                expect(Array.isArray(data)).to.equal(true);
+                done();
+            });
+        });
+
+        it('get a template content', function(done){
+            client.get('/templates/snapp/' + fileId[0] + '/template', function(err, req, res, data){
+                expect(new Buffer(data, 'base64').toString()).to.equal(readTemplate.toString());
+                done();
+            });
+        });
+
+    });
+
+    describe('snapp merge and file test', function(){
+
+        it('merge and fill forms', function(done){
+            var sampleArray = _.map(fileId, function(id){
+                return {
+                    templateId: id,
+                    formOrder: fileId.indexOf(id),
+                    mappings: formMapping
+                }
+            });
+
+            client.post('/mergefill/snapp', sampleArray, function(err, req, res, data){
+                console.log(data);
+                done();
+            });
+
+        });
+    });
+});
